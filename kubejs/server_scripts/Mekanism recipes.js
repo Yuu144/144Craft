c6fb5a71e04e6d87abf13ad260d2eea0e3b0ff28
Ikaros144 <shizukaros@gmx.net>
2024-12-30 01:10:37 +0100

    Mekanism Crafting script

--- kubejs/server_scripts/Mekanism recipes.js
+++ kubejs/server_scripts/Mekanism recipes.js
@@ -1,6 +1,6 @@
 //onEvent('recipes', event => {
 
-//Mekanism Start
+//Mekanism Start create
 ServerEvents.recipes(event => {
     //Gas Burning Generator
     event.shaped('mekanismgenerators:gas_burning_generator', [
@@ -15,9 +15,35 @@ ServerEvents.recipes(event => {
     })
 })
 
+//Mekanism ADVANCED create
+ServerEvents.recipes(event => {
+    //Atomic Disassembler
+    event.shaped('mekanism:atomic_disassembler',[
+        'ABA',
+        'ACA',
+        ' D '
+    ],{
+        A:'mekanism:alloy_reinforced',
+        B:'mekanism:basic_induction_cell',
+        C:'mekanism:alloy_atomic',
+        D:'mekanism:ingot_refined_obsidian'
+    })
+
+
+})
+
+//Mekanism start delete
 ServerEvents.recipes(event => {
     [
         'mekanismgenerators:generator/gas_burning'
+    ]
+    .forEach((recipeID) => event.remove({id: recipeID}));
+})
+
+//Mekanism ADVANCED delete
+ServerEvents.recipes(event => {
+    [
+        'mekanism:atomic_disassembler'
     ]
     .forEach((recipeID) => event.remove({id: recipeID}));
 })
