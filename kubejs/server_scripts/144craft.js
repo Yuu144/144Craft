1a9567cab6e82d2f2da2502aa067ad95c022f6b4
Ikaros144 <shizukaros@gmx.net>
2025-01-19 17:13:13 +0100

    Custom recipes

--- kubejs/server_scripts/144craft.js
+++ kubejs/server_scripts/144craft.js
@@ -1,3 +1,54 @@
 ServerEvents.tags('item', event => {
     event.add('forge:elements/144-ium', 'kubejs:element_144-ium')
   })
+
+
+ServerEvents.recipes(hscraft => {
+    //-null- Crafting
+    hscraft.custom({
+      type: "create:mechanical_crafting",
+      pattern: [
+      '    O    ',
+      '   FEF   ',
+      '   EBE   ',
+      ' FEADAEF ',
+      'OEBDCDBEO',
+      ' FEADAEF ',
+      '   EBE   ',
+      '   FEF   ',
+      '    O    '
+      ],
+      key: {
+        O: Ingredient.of('create:cogwheel').toJson(),
+        A: Ingredient.of('draconicevolution:chaos_shard').toJson(),
+        B: Ingredient.of('draconicevolution:large_chaos_frag').toJson(),
+        C: Ingredient.of('kubejs:pure_element_144-ium').toJson(),
+        D: Ingredient.of('cataclysm:ignitium_block').toJson(),
+        E: Ingredient.of('draconicevolution:awakened_draconium_block').toJson(),
+        F: Ingredient.of('mekanism:pellet_antimatter').toJson()
+        
+      },
+      result: Ingredient.of('kubejs:-null-').toJson(),
+      acceptMirrored: false
+    }).id('144craft:kubejs/-null-')
+
+    //pure 144ium
+    hscraft.custom({
+      "type": "mekanism:nucleosynthesizing",
+      "itemInput": {
+        "amount": 1,
+        "ingredient": {
+          "item": "kubejs:element_144-ium"
+        }
+      },
+      "gasInput": {
+        "amount": 10,
+        "gas": "mekanism:antimatter"
+      },
+      "output": {
+        "count": 1,
+        "item": "kubejs:pure_element_144-ium"
+      },
+      "duration": 5000
+    }).id('144craft:nucleosynthesizing/pure_144-ium')
+})
--- kubejs/server_scripts/Mekanism recipes.js
+++ kubejs/server_scripts/Mekanism recipes.js
@@ -28,7 +28,42 @@ ServerEvents.recipes(event => {
         C:'mekanism:alloy_atomic',
         D:'mekanism:ingot_refined_obsidian'
     })
+    
+    //Reactor Controller
+    event.shaped('mekanismgenerators:fusion_reactor_controller',[
+        'ABA',
+        'CDC',
+        'CCC'
+    ],{
+        A:'mekanism:ultimate_control_circuit',
+        B:'cataclysm:ignitium_ingot',
+        C:'mekanismgenerators:fusion_reactor_frame',
+        D:'mekanism:basic_chemical_tank'
+    })
 
+    //Fission Fuel Assembly
+    event.shaped('mekanismgenerators:fission_fuel_assembly',[
+        'ABA',
+        'ACA',
+        'ABA'
+    ],{
+        A:'#forge:ingots/lead',
+        B:'#forge:ingots/steel',
+        C:'cataclysm:witherite_ingot'
+    })
+
+    //Supercharged Coil
+    event.shaped('mekanism:supercharged_coil',[
+        'ABA',
+        'CDC',
+        'EEE'
+    ],{
+        A:'minecraft:copper_ingot',
+        B:'kubejs:element_144-ium',
+        C:'mekanism:ultimate_control_circuit',
+        D:'mekanism:laser',
+        E:'mekanism:pellet_polonium'
+    })
 
 })
 
@@ -43,7 +78,10 @@ ServerEvents.recipes(event => {
 //Mekanism ADVANCED delete
 ServerEvents.recipes(event => {
     [
-        'mekanism:atomic_disassembler'
+        'mekanism:atomic_disassembler',
+        'mekanismgenerators:reactor/controller',
+        'mekanismgenerators:fission_reactor/fuel_assembly',
+        'mekanism:supercharged_coil'
     ]
     .forEach((recipeID) => event.remove({id: recipeID}));
 })
