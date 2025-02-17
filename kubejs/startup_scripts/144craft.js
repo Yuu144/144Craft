9281a63d4b638b0dffeaab0258f74c1eaab43e25
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2025-02-17 16:40:18 +0100

    Item Updates

--- kubejs/startup_scripts/144craft.js
+++ kubejs/startup_scripts/144craft.js
@@ -1,9 +1,19 @@
 StartupEvents.registry('item', event => {
-  event.create('pure_element_144-ium')
+  event.create('pure_element_144_ium')
   event.create('-null-')
   event.create('orb_of_eternity')
   event.create('void_crystal')
   event.create('neutron_star_shard')
   event.create('time_manipulator')
   event.create('ethereal_capacitor')
+})
+
+StartupEvents.modifyCreativeTab('ores144craft:ores144craft_tab', event => {
+	event.addAfter('', 'kubejs:pure_element_144_ium')
+  event.addAfter('', 'kubejs:-null-')
+  event.addAfter('', 'kubejs:orb_of_eternity')
+  event.addAfter('', 'kubejs:void_crystal')
+  event.addAfter('', 'kubejs:neutron_star_shard')
+  event.addAfter('', 'kubejs:time_manipulator')
+  event.addAfter('', 'kubejs:ethereal_capacitor')
 })
