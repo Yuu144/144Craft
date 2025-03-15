94d58e8e7b6264d86a8716a81aaf219bd12df91e
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2025-03-15 15:19:47 +0100

    Paul is ein gooner

---
+++ kubejs/startup_scripts/weapons.js
@@ -0,0 +1,29 @@
+ItemEvents.modification(event => {
+    event.modify('soulsweapons:darkin_blade', item => {
+        item.attackDamage = 21
+    })
+
+    event.modify('soulsweapons:nightfall', item => {
+        item.attackDamage = 21
+    })
+
+    event.modify('soulsweapons:darkin_scythe', item => {
+        item.attackDamage = 21
+    })
+
+    event.modify('soulsweapons:shadow_assasin_scythe', item => {
+        item.attackDamage = 21
+    })
+
+    event.modify('soulsweapons:withered_wabbajack', item => {
+        item.attackDamage = 16
+    })
+
+    event.modify('soulsweapons:soul_reaper', item => {
+        item.attackDamage = 20
+    })
+
+    event.modify('soulsweapons:forlorn_scythe', item => {
+        item.attackDamage = 20
+    })
+})
