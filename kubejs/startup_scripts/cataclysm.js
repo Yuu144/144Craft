3970ae60ee435352105348bad30af5fcf0ae09f2
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2025-11-07 20:48:04 +0100

    Cataclysm Weapons

--- kubejs/startup_scripts/cataclysm.js
+++ kubejs/startup_scripts/cataclysm.js
@@ -23,4 +23,36 @@ ItemEvents.modification(event => {
     item.armorProtection = 11
     item.armorToughness = 4
   })
+
+  event.modify('cataclysm:infernal_forge', item => {
+    item.attackDamage = 14
+  })
+
+  event.modify('cataclysm:void_forge', item => {
+    item.attackDamage = 15
+  })
+
+  event.modify('cataclysm:meat_shredder', item => {
+    item.attackDamage = 11
+  })
+
+  event.modify('cataclysm:soul_render', item => {
+    item.attackDamage = 18.5
+  })
+
+  event.modify('cataclysm:ancient_spear', item => {
+    item.attackDamage = 16
+  })
+
+  event.modify('cataclysm:astrape', item => {
+    item.attackDamage = 18
+  })
+
+  event.modify('cataclysm:ceraunus', item => {
+    item.attackDamage = 24
+  })
+
+  event.modify('cataclysm:the_incinerator', item => {
+    item.attackDamage = 21
+  })
 })
