9b3cd6880b939d0fc842521a91704b9400f07132
Ikaros144 <shizukaros@gmx.net>
2025-11-07 20:47:15 +0100

    Armor updates

--- kubejs/startup_scripts/cataclysm.js
+++ kubejs/startup_scripts/cataclysm.js
@@ -1,12 +1,32 @@
 ItemEvents.modification(event => {
+  event.modify('cataclysm:ignitium_helmet', item => {
+    item.armorProtection = 9
+    item.armorToughness = 7
+    item.armorKnockbackResistance = 0.2
+  })
+
   event.modify('cataclysm:ignitium_chestplate', item => {
     item.armorProtection = 14
-    item.armorToughness = 6
+    item.armorToughness = 7
+    item.armorKnockbackResistance = 0.2
   })
 
   event.modify('cataclysm:ignitium_elytra_chestplate', item => {
     item.armorProtection = 14
-    item.armorToughness = 6
+    item.armorToughness = 7
+    item.armorKnockbackResistance = 0.1
+  })
+
+  event.modify('cataclysm:ignitium_leggings', item => {
+    item.armorProtection = 12
+    item.armorToughness = 7
+    item.armorKnockbackResistance = 0.2
+  })
+
+  event.modify('cataclysm:ignitium_boots', item => {
+    item.armorProtection = 9
+    item.armorToughness = 7
+    item.armorKnockbackResistance = 0.2
   })
 
   event.modify('cataclysm:monstrous_helm', item => {
