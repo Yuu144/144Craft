7bafbabaf782773677817d7da948c8d376249d64
Yuu144 <97678250+Yuu144@users.noreply.github.com>
2025-11-07 22:26:33 +0100

    Item Rarities

--- kubejs/startup_scripts/item_rarities.js
+++ kubejs/startup_scripts/item_rarities.js
@@ -10,4 +10,20 @@ ItemEvents.modification(event => {
   event.modify('silentgear:crimson_steel_ingot', item => {
     item.rarity = 'RARE'
   })
+
+  event.modify('bhc:red_heart', item => {
+    item.rarity = 'RARE'
+  })
+
+  event.modify('bhc:yellow_heart', item => {
+    item.rarity = 'EPIC'
+  })
+
+  event.modify('bhc:green_heart', item => {
+    item.rarity = 'EPIC'
+  })
+
+  event.modify('bhc:blue_heart', item => {
+    item.rarity = 'EPIC'
+  })
 })
