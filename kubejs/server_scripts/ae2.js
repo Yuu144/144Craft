6818d384b0b4534079bfbdb80434a302dc86039c
Ikaros144 <shizukaros@gmx.net>
2025-11-09 15:42:39 +0100

    last touches part 2

--- kubejs/server_scripts/ae2.js
+++ kubejs/server_scripts/ae2.js
@@ -12,6 +12,18 @@ ServerEvents.recipes(event => {
         B:'minecraft:piston',
         C:'minecraft:copper_ingot'
     })
+
+    //Creative Storage
+    event.shaped('ae2:creative_item_cell', [
+        'ABA',
+        'BCB',
+        'DDD'
+    ],{
+        A:'ae2:quartz_vibrant_glass',
+        B:'ae2:sky_dust',
+        C:'kubejs:-null-',
+        D:'megacells:item_storage_cell_256m'
+    })
 })
 
 //remove
